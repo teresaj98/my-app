41a608f869b6d424b929901e0a56382107f997b7
Teresa <Teresajerard98@gmail.com>
2026-04-21 16:13:27 +0530

    Add components for event management, including EventCard, ExploreBtn, Navbar, and LightRays. Introduce constants for event data and update layout and styles for improved UI.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -25,9 +27,26 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}>
+       <Navbar />
+        <div className="absolute top-0 inset-0 min-h-screen z-[-1]">
+        <LightRays
+          raysOrigin="top-center-offset"
+          raysColor="#5dfeca"
+          raysSpeed={0.5}
+          lightSpread={0.9}
+          rayLength={1.4}
+          followMouse={true}
+          mouseInfluence={0.02}
+          noiseAmount={0}
+          distortion={0.01}
+      />
+      </div>
+      <main>
+        {children}
+      </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
+import EventCard from "@/components/EventCard";
+import { ExploreBtn } from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
+
 const Home = () => {
   return (
-    <h1>Welcome to Home</h1>
+    <section>
+      <h1 className="text-center">The Hub for Every Dev <br /> Event You Cannot Miss</h1>
+      <p className="text-center mt-5">Hackathons, Conferences, Meetups, and More.</p>
+      
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events.map((event) => (
+            <li className="list-none" key={event.slug}>
+              <EventCard title={event.title} image={event.image} slug={event.slug} location={event.location} date={event.date} time={event.time} description={event.description} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   )
 }
 
